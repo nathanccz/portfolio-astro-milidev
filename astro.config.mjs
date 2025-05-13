2dc9812b0871b4112e21a71c41cec7e5b41c1c4b
Nathan Casarez <nathanweb.dev@proton.me>
2025-05-13 16:17:36 -0700

    Final changes before go live.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://bartoszlenar.github.io",
-  base: "/astro-milidev/",
+  site: "https://nathancc.netlify.app",
+  base: "/",
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
     shikiConfig: {
